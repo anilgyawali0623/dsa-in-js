017d74cb3c2336233d63f7768d4ebd9bff0c4b69
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-06 17:19:10 +0545

    search

---
+++ ongoing/tutorial5.js
@@ -0,0 +1,21 @@
+// search element in array
+
+// make Array
+// use loop to search Element
+// search element with text input and button
+//  shortcut for search element
+//  shortcut for delete element
+
+let data = [20, 3, 4, 233, 43, 54, 12, 32, 43, 70];
+let items = 70;
+let index = 1;
+for (let i = 0; i < data.length; i++) {
+  if (data[i] === items) {
+    index ++;
+      break;
+  }
+}
+
+ 
+
+ console.log(index);
