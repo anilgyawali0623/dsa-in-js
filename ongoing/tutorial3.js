aec024845e7ea8e086bb152440be0c5f95ee7ec7
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-06 11:56:57 +0545

    deleting the element in the array

--- ongoing/tutorial3.js
+++ ongoing/tutorial3.js
@@ -4,9 +4,9 @@ let position = 2;
 for (let i = data.length - 1; i >= 0; i--) {
   if (i >= position) {
     data[i + 1] = data[i];
-    if (i === position) {
-      data[i] = newVal;
-    }
+     if(i===position){
+         data[i]=newVal
+     }
   }
 }
 console.log(data);
