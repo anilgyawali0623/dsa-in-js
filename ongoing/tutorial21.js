5cfcf60dacbb103681ca0509368e3c227885eb1f
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-16 22:34:41 +0545

    binary search iterative way

--- ongoing/tutorial21.js
+++ ongoing/tutorial21.js
@@ -1,1 +1,20 @@
 // binary search
+// array must be sorted
+
+let data = [4, 9, 13, 17];
+let start = 0;
+let find = 17;
+let end = data.length - 1;
+let position = undefined;
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  if (data[mid] === find) {
+    position = mid;
+    break;
+  } else if (data[mid] < find) {
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+console.log(position);
