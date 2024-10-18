7f1a41976324de5d8577dd9193fa858aebc33d41
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-18 12:25:10 +0545

    palindrome

---
+++ ongoing/tutorial26.js
@@ -0,0 +1,17 @@
+// palindrome
+
+function palindrome(data) {
+  let start = 0;
+  let end = data.length - 1;
+  let result = true;
+  while (end > start) {
+    if (data[start] != data[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return result;
+}
+let str = "level";
+console.log(palindrome(str));
