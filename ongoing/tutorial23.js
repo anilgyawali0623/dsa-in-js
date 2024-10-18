20fcbedc395f7e1fd163f3571483f46aeb3eaed0
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-18 11:51:56 +0545

    insertion sort

---
+++ ongoing/tutorial23.js
@@ -0,0 +1,21 @@
+// selection sort
+
+let items = [2,22,4,6,2, 4, 5, 56, 23, 52];
+function selectionSort(data) {
+  let minId;
+  for (let i = 0; i < data.length; i++) {
+    minId = i;
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[j] < data[minId]) {
+        minId = j;
+      }
+    }
+    let temp = data[minId];
+    data[minId] = data[i];
+    data[i] = temp;
+  }
+}
+
+
+ selectionSort(items)
+  console.log(items)
---
+++ ongoing/tutorial24.js
@@ -0,0 +1,18 @@
+// insertion sort
+
+let arr = [2,9,4,3,15,11,16];
+function insertionSort(arr) {
+  let i, current, j;
+  for (i = 1; i < arr.length; i++) {
+    current = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+}
+
+ insertionSort(arr);
+  console.log(arr)
