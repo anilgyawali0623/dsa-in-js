b09b0997a8916ed45991a37d2948616cbcc6f13b
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-21 15:27:45 +0545

    traversing the node

---
+++ ongoing/tutorial31.js
@@ -0,0 +1,42 @@
+// linked list
+// node is in the form of the object
+
+// first make the head which is basically an object
+// value and next
+// value ma value hunxa next ma address hunxa
+
+class List {
+  constructor(data) {
+    this.head = {
+      next: null,
+      value: data,
+    };
+    this.tail = this.head;
+    this.size = 1;
+  }
+
+  appendNode(nodeData) {
+    let newNode = {
+      value: nodeData,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size++;
+  }
+  traversing() {
+    let counter = 0;
+    let currentNode = this.head; // first node
+
+    while (counter < this.size) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+  }
+}
+
+let list = new List(23);
+list.appendNode(200);
+list.appendNode(400);
+
+console.log(list);
