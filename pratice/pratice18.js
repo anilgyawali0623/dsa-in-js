a143b31eb5cdf8a333e968a897a0b2b9e1bfade7
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-10-24 23:12:02 +0545

    deleting the node

--- pratice/pratice18.js
+++ pratice/pratice18.js
@@ -28,6 +28,22 @@ class LinkedList {
       counter++;
     }
   }
+
+  deleteNode(index) {
+    let counter = 1;
+    let lead = this.head;
+    if (index === 1) {
+      this.head = this.head.next;
+    } else {
+      while (counter < index - 1) {
+        //  to find the lead node we do index-1;
+        lead = lead.next;
+        counter += 1;
+      }
+      let nextNode = lead.next.next;
+      lead.next = nextNode;
+    }
+  }
 }
 
 let list = new LinkedList(200);
