ab7558a56725797fd9c3979bd66b79d2d8124478
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-11-07 17:58:07 +0545

    breath first search traversing

--- ongoing/tutorial33.js
+++ ongoing/tutorial33.js
@@ -67,9 +67,20 @@ preOrder(root){
   }
 }
 
- inOrder(root){
-   
- }
+bfsTraversing(){
+ let queue=[];
+ queue.push(this.root)
+  while(queue.length){
+    let current= queue.shift();
+    if(current.left){
+       queue.push(current.left);
+    }
+     if(current.right){
+      queue.push(current.right); 
+     }
+
+  }
+}
 
 
 
